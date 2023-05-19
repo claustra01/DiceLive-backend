9ba02548cddf372a4d2eb4f4307432728396fc8d
pei <youappleyuhei1414@outlook.jp>
2023-05-20 07:18:51 +0900

    pei0520

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
+  controllers: [AppController,],
+  providers: [AppService],
   imports: [UserModule],
 })
-export class AppModule {}
+export class AppModule { }
+
