6aa91217c8567251d34cf2666a484f75d4e16602
claustra01 <shinya430.g@gmail.com>
2023-05-20 15:36:07 +0900

    update: password hashed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { User, Prisma } from '@prisma/client';
@@ -7,6 +8,8 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const hash = await bcrypt.hash(data.password, 10)
+    data.password = hash
     return this.prisma.user.create({
       data,
     });
