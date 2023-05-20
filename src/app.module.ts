0db573e6a23b96afac2914d1c20f34e27d7f02c4
claustra01 <shinya430.g@gmail.com>
2023-05-20 16:48:10 +0900

    add: simple login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
+  providers: [AuthService],
 })
 export class AppModule {}
