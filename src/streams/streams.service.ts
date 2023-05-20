193498587cfcf0a40020af48fffca9413296e42c
claustra01 <shinya430.g@gmail.com>
2023-05-20 21:02:55 +0900

    update: error handling

--- src/streams/streams.service.ts
+++ src/streams/streams.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { Stream, Prisma} from '@prisma/client';
+import { Stream, Prisma } from '@prisma/client';
 
 @Injectable()
 export class StreamsService {
     constructor(private prisma: PrismaService) {}
 
+    async getStreamByUrl(url: string): Promise<Stream | null> {
+      return this.prisma.stream.findUnique({
+        where: { url }
+      });
+    }
+
     async createStream (data: Prisma.StreamCreateInput): Promise<Stream> {
+        const stream = await this.getStreamByUrl(data.url);
+        if (stream) {
+          throw new BadRequestException;
+        }
+
         return await this.prisma.stream.create({
           data
         });
